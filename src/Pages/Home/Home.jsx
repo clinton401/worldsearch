bbb4076f12cefac7a9d0c0a305720b1d2b3545db
clinton <clintonphillips464@gmail.com>
2024-01-17 00:24:13 +0100

    fiexed the pagination errors and added scroll to top button

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,15 +6,30 @@ import Card from "../../Components/Card";
 import InputSkeleton from "../../Components/InputSkeleton";
 import Input from "../../Components/Input";
 import ReactPaginate from "react-paginate";
+import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { faAngleLeft, faAngleRight, faArrowUp } from "@fortawesome/free-solid-svg-icons";
+const rightAnimation = {
+  hidden: {
+    x: 500,
+    opacity: 0,
+  },
+  visible: {
+    x: "0",
+    opacity: 1,
+    transition: {
+      duration: 0.3,
+      ease: "easeInOut",
+    },
+  },
+};
 function Home() {
   const [noC, setNoC] = useState(false);
   const [newData, setNewData] = useState([]);
   const [itemOffset, setItemOffset] = useState(0);
   const [pageCount, setPageCount] = useState(0);
+  const [backToTop, setBackToTop] = useState(false);
   const itemsPerPage = 30;
-
   const {
     lightMode,
     data,
@@ -35,17 +50,52 @@ function Home() {
     }
   }
   numHandler();
+  const handleScroll = () => {
+    const scrollY = window.scrollY;
+
+    // Set the visibility based on the scroll position
+    setBackToTop(scrollY > 100);
+  };
+
+  // Attach scroll event listener when the component mounts
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // Function to scroll to the top when the button is clicked
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
   useEffect(() => {
     if (data) {
       const endOffset = itemOffset + itemsPerPage;
+      if (data.length < itemOffset) {
+        setItemOffset(0)
+      }
+      
       setNewData(data.slice(itemOffset, endOffset));
       setPageCount(Math.ceil(data.length / itemsPerPage));
     }
   }, [itemOffset, itemsPerPage, data]);
 
   const handlePageClick = (event) => {
-    const newOffset = (event.selected * itemsPerPage) % data.length;
-    setItemOffset(newOffset);
+    // setActive(false)
+    // setTimeout(() => {
+       const newOffset = (event.selected * itemsPerPage) % data.length;
+
+       console.log(event);
+       setItemOffset(newOffset);
+       
+    // }, 1000);
+   
     
   };
   return (
@@ -116,6 +166,21 @@ function Home() {
           nextLinkClassName="page-num"
           activeLinkClassName="actives"
         />
+      )}
+      {backToTop && (
+        <motion.span
+          className={`scroll-btn ${backToTop ? "acti" : ""}`}
+          onClick={scrollToTop}
+          variants={rightAnimation}
+          initial="hidden"
+          animate="visible"
+        >
+          <button className="butt">
+            <svg className="svgIcon" viewBox="0 0 384 512">
+              <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"></path>
+            </svg>
+          </button>
+        </motion.span>
       )}
     </main>
   );
